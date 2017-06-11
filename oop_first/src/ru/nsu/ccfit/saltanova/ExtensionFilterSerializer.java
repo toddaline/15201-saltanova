e2e298368d03a8adf41588909a641c8095874864
Victoria <todd13777@gmail.com>
2017-06-11 17:05:11 +0700

    fixed bugs

--- oop_first/src/ru/nsu/ccfit/saltanova/ExtensionFilterSerializer.java
+++ oop_first/src/ru/nsu/ccfit/saltanova/ExtensionFilterSerializer.java
@@ -5,7 +5,7 @@ public class ExtensionFilterSerializer implements ISerializer {
     @Override
     public ExtensionFilter serialize(String line) throws FilterSerializerException {
         if (line.length()<2) {
-            throw new FilterSerializerException("String with prefix '<' is too short");
+            throw new FilterSerializerException("String with prefix '.' is too short");
         }
         return new ExtensionFilter(line);
     }
