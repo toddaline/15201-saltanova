acd1b7786993483a583f98606480d47b280b6654
Victoria <todd13777@gmail.com>
2017-06-13 14:41:33 +0700

    added total statistics

--- oop_second/src/ru/nsu/ccfit/saltanova/AppController.java
+++ oop_second/src/ru/nsu/ccfit/saltanova/AppController.java
@@ -23,4 +23,20 @@ public class AppController {
     public static void setCar(int num) {
         app.setNumberOfCars(num);
     }
+    
+     public static void setBodyTotal(int num) {
+        app.setBodiesTotal(num);
+    }
+
+    public static void setEngineTotal(int num) {
+        app.setEnginesTotal(num);
+    }
+
+    public static void setAccessoryTotal(int num) {
+        app.setAccessoriesTotal(num);
+    }
+
+    public static void setCarTotal(int num) {
+        app.setCarsTotal(num);
+    }
 }
