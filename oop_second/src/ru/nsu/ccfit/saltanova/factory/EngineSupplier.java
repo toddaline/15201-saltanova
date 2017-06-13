0353bd906ed208df63416a8e54e4b7b7cb6f400d
Victoria <todd13777@gmail.com>
2017-06-13 15:38:07 +0700

    changed int to AtomicInteger

--- oop_second/src/ru/nsu/ccfit/saltanova/factory/EngineSupplier.java
+++ oop_second/src/ru/nsu/ccfit/saltanova/factory/EngineSupplier.java
@@ -3,14 +3,15 @@ package ru.nsu.ccfit.saltanova.factory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import static ru.nsu.ccfit.saltanova.AppController.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.Thread.sleep;
+import static ru.nsu.ccfit.saltanova.AppController.*;
 
 public class EngineSupplier implements Runnable {
 
     private static final Logger log = LogManager.getLogger("logger");
-    private static int bodyID = 0;
+    AtomicInteger engineID = new AtomicInteger(0);
     private int timeout;
     private Stock<Engine> stock;
 
@@ -28,10 +29,10 @@ public class EngineSupplier implements Runnable {
         try {
             while (true) {
                 sleep(timeout);
-                Engine newEngine = new Engine(bodyID++);
+                Engine newEngine = new Engine(engineID.incrementAndGet());
                 stock.put(newEngine);
                 setEngine(stock.getSize());
-                setEngineTotal(engineID);
+                setEngineTotal(engineID.get());
             }
         } catch (InterruptedException e) {
             log.info("EngineSupplier was interrupted");
